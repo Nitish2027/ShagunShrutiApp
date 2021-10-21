78cadf4bf23fa5551e42e987414dc86a7025898c
Nitish Singh <nitish2027@gmail.com>
2021-10-22 03:25:50 +0530

    First Commit. Setting up the app.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA-1arWyZHJTIpg8dahl4uA-pOxAG0GS40",
+    authDomain: "shagunshrutiapp.firebaseapp.com",
+    projectId: "shagunshrutiapp",
+    storageBucket: "shagunshrutiapp.appspot.com",
+    messagingSenderId: "927986517501",
+    appId: "1:927986517501:web:e1dbefdab0c547a83f8594",
+    measurementId: "G-YZC7TSPMYY"
+  }
+};
